d88b321b17c335b49f793d5a0c774abde6e17b2d
GeeklifeWang <1018752647@qq.com>
2016-03-02 12:17:33 +0800

    update

--- my python note/continue用法.py
+++ my python note/continue用法.py
@@ -1,11 +1,13 @@
 #-*- coding:utf-8 -*-
 
 #continue用法
+#不执行后边的语句，直接开始新的循环
 while True:
     A=raw_input('enter:')
     if int(A)==1 :
         print 'keep'
         continue
+        break
     else:
         print int(A)
 
