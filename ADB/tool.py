6be585f40bed6e354f18064539f5790e88c1ed24
GeeklifeWang <1018752647@qq.com>
2016-03-08 13:32:08 +0800

    update

--- ADB/tool.py
+++ ADB/tool.py
@@ -1,6 +1,6 @@
 # -*- coding:gbk -*-
 _author = 'wangyouwei'
-import os, sys
+import os, sys,re
 from pyadb.adb import ADB
 
 def has(arg):
@@ -37,7 +37,6 @@ def install_app():
         stdout = os.popen(r'adb install %s/%s' % (path,filelist[int(choice)]))
         print '    ',stdout.read()
 
-
 def del_weather():
     stdout = os.popen('adb uninstall com.moji.mjweather')
     os.popen(r'adb shell rm -r sdcard/moji')
@@ -54,9 +53,31 @@ def get_cpu(arg):
     myadb.shell_command('top -n 5|grep %s' % arg)
     print myadb.get_output()
 
+
 def get_memory(arg):
     stdout = os.popen('adb shell dumpsys meminfo %s' % arg)
     print '    ',stdout.read()
+
+def get_liuliang(arg):
+    myadb.shell_command('top -n 2|grep %s' % arg)
+    deal_list=myadb.get_output().split(' ')
+    for i in deal_list:
+        if i.startswith('u0_'):
+            result_str = i
+            #print '    ',result_str
+            break
+    m = re.match(r'u0_a(\d+)',result_str)
+    uid = m.group(1)
+    choice = raw_input('    是否清除原始流量数据? y/n:')
+    if choice.lower() == 'y':
+        os.popen(r'adb shell rm -rf /proc/uid_stat/10%s/*' % uid)
+    #print '    ',uid
+    myadb.shell_command(r'cat /proc/uid_stat/10%s/tcp_rcv' % uid)
+    receive = myadb.get_output()
+    myadb.shell_command(r'cat /proc/uid_stat/10%s/tcp_snd' % uid)
+    send = myadb.get_output()
+    final = (int(receive) - int(send))/1024
+    print '    ',final,'MB'
 
 def run(arg):
     if arg==1:
@@ -72,20 +93,24 @@ def run(arg):
         if check_paper():
             del_paper()
     if arg==6:
-        print '   ',66666666666
-    if arg==7:
         if check_weather():
-            get_cpu('com.moji.mjweather')
+            get_liuliang('com.moji.mjweather')
+    if arg==7:
+        if check_paper():
+            get_liuliang('com.moji.wallpaper')
     if arg==8:
         if check_weather():
-            get_memory('com.moji.mjweather')
+            get_cpu('com.moji.mjweather')
     if arg==9:
-        if check_paper():
-            get_cpu('com.moji.wallpaper')
+        if check_weather():
+            get_memory('com.moji.mjweather')
     if arg==10:
         if check_paper():
-            get_memory('com.moji.wallpaper')
+            get_cpu('com.moji.wallpaper')
     if arg==11:
+        if check_paper():
+            get_memory('com.moji.wallpaper')
+    if arg==12:
         sys.exit()
 
 def main():
@@ -96,12 +121,13 @@ def main():
     3.安装墨迹天气或墨迹壁纸
     4.卸载墨迹天气
     5.卸载墨迹壁纸
-    6.
-    7.查询墨迹天气占用cpu信息
-    8.查询墨迹天气占用内存信息
-    9.查询墨迹壁纸占用cpu信息
-    10.查询墨迹壁纸占用内存信息
-    11.退出
+    6.查询墨迹天气消耗流量
+    7.查询墨迹壁纸消耗流量
+    8.查询墨迹天气占用cpu信息
+    9.查询墨迹天气占用内存信息
+    10.查询墨迹壁纸占用cpu信息
+    11.查询墨迹壁纸占用内存信息
+    12.退出
     ***************MY TOOLS***************
     '''
     select = raw_input('    请输入数字选择功能：')
@@ -118,7 +144,6 @@ def main():
 
 if __name__ == '__main__':
     myadb = ADB('E:\\adt-bundle-windows\\sdk\\platform-tools\\adb')
-
     while True:
         applist = os.popen(r'adb shell pm list package').read()
         main()
