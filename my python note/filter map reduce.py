d7f15c02b47dd2aaf1d43480c1ef474b823c7338
geeklifewang <1018752647@qq.com>
2017-02-07 16:28:51 +0800

    update

--- my python note/filter map reduce.py
+++ my python note/filter map reduce.py
@@ -1,30 +1,42 @@
 # -*- coding:utf8 -*-
 
-from operator import add
+'filter(func,iterable) 需要两个参数 第一个为自定义函数 第二个位可迭代的序列 序列中的元素逐一传到自定义函数处理 函数返回为True添加到新的结果列表'
 
-#print filter(int,'123'[0]),type(filter(int,'123'[0]))
-print filter(int,'123'),type(filter(int,'123'))
+def myfunc1(arg):
+    if type(arg) == int:
+        return arg
+l1 = [11,1,'2','s']
+print filter(myfunc1,l1)
 
-print map(int,['1','2','3']),type(map(int,[1,2,3]))
 
-print reduce(add,'123','a'),type(reduce(add,'123','a'))
+'map(func,iterable)    需要两个参数 第一个为自定义函数 第二个位可迭代的序列 序列中的元素逐一传到自定义函数处理 返回的结果添加到新的结果列表'
+def myfunc2(arg):
+    if type(arg) == int:
+        return arg+1
 
-'''
-#map
-def add(obj):
-    return obj+1
-print map(add,[1,2,3])
+l2 = [1,'2',3]
+print map(myfunc2,l2)
 print map(lambda x,y:x+y,[1,2,3],[10,10,10])
 
-#reduce
-from functools import reduce
+
+'''reduce(func,iterable) 需要三个参数 第一个为自定义函数(必须接受两个参数) 第二个位可迭代的序列
+第三个为初始值(如果不传迭代序列第一个元素作为初始值)
+自定义函数处理的结果作为arg1和迭代序列的下一个元素作为arg2继续执行函数 直到序列迭代完 返回一个最终值'''
+def myfunc3(arg1,arg2):
+    return arg1.upper()+arg2
+l3 = ['a','bc','def']
+print reduce(myfunc3,l3)
+print reduce(myfunc3,l3,'z')
+
+
 def str1int(s):
     def fn(x, y):
+        print x,y
         return x * 10 + y
 
     def char1num(s):
         return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
     return reduce(fn, map(char1num, s))
-print str1int('1')
-'''
+print str1int('123')
+
 
